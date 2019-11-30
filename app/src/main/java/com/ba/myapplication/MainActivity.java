5f8321ecf33e6b8dfec4c59fb43a5caed00faadc
unknown <aliakeel27@yahoo.com>
2019-11-30 14:07:26 +0300

    Initial commit

---
+++ app/src/main/java/com/ba/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.ba.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int i = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView t = findViewById(R.id.tv);
+
+        Button btn = findViewById(R.id.button1234);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t.setText("hello , Abo Hasan" + i++);
+                Log.e("tag","hello , Abo Hasan");
+//                Intent myIntent = new Intent(CurrentActivity.this, NextActivity.class);
+//                myIntent.putExtra("key", value); //Optional parameters
+//                CurrentActivity.this.startActivity(myIntent);
+
+            }
+        });
+
+    }
+}
